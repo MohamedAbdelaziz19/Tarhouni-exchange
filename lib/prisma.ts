3d4d71f1aa7d92e1dfbf96b5c9c0c740c79d85a8
MohamedAbdelaziz19 <davidpunisher475@gmail.com>
2025-02-27 11:35:41 +0100

    New Update 5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,9 @@
-//lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-/* eslint-disable no-var */
-declare global {
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-/* eslint-enable no-var */
-
-export const prisma = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
