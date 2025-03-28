c61b6dde474a91c498ef532891cbe9eb66534b99
MohamedAbdelaziz19 <davidpunisher475@gmail.com>
2025-03-28 12:38:29 +0100

    change Meta data 6

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,25 @@
-module.exports = {
-    siteUrl: "http://tarhouni-exchange.com",
-    generateRobotsTxt: true,
-  };
-  
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.tarhouni-exchange.com/",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+    {
+      url: "https://www.tarhouni-exchange.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.tarhouni-exchange.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    // Ajoute d'autres URLs si nécessaire
+  ];
+}
