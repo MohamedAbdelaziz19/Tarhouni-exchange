2113d30c712c16e869ee290da85de46b0a8eb9a3
MohamedAbdelaziz19 <davidpunisher475@gmail.com>
2025-03-28 12:35:23 +0100

    change Meta data 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,15 @@ const saira = Saira({
  })
 
  export const metadata: Metadata = {
-  title: "Tarhouni Exchange | Meilleur taux de change en Tunisie",
+  title:{
+    default:"Tarhouni Exchange ",
+    template:'%s | Meilleur taux de change en Tunisie'
+  },
   description: "Obtenez les meilleurs taux de change en Tunisie avec Tarhouni Exchange. Convertissez vos devises rapidement et en toute sécurité.",
   keywords: "taux de change, Tunisie, devises, Tarhouni Exchange",
+  other: {
+    "google-site-verification": "4ywGfxBvev4knam6BU3zVuyPryLW8qfFfBN_ktakZaI",
+  },
   icons: {
     icon: "/favicon.ico",
   },
@@ -45,6 +51,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+            <head>
+        {/* Schéma JSON-LD */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Tarhouni Exchange",
+              "url": "https://www.tarhouni-exchange.com",
+              "logo": "https://www.tarhouni-exchange.com/logo.png",
+              "description": "Obtenez les derniers taux de change en Tunisie.",
+              "address": {
+                "@type": "5080",
+                "streetAddress": "Avenue, Tabulbah 5080",
+                "addressLocality": "Tabulbah",
+                "addressCountry": "TN"
+              },
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+21629 450 786",
+                "contactType": "customer service"
+              }
+            }),
+          }}
+        />
+      </head>
       <body
         className={saira.className}
       >
